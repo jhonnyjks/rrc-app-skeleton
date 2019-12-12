cd0be5e14ee3422ddd36d666f0164af9a41a5d8a
Jhonny Mesquita <jhonnyjks@gmail.com>
2019-12-11 23:48:55 -0300

    Remoção do crud de usuários e perfis do arquivo exports.js

--- exports.js
+++ exports.js
@@ -1,6 +1,4 @@
 import Dashboard from './dashboard/dashboard'
-import User from '../users/index'
-import Profile from '../profiles/index'
 
 // Reducers do projeto
 export const reducers = {
@@ -10,19 +8,16 @@ export const reducers = {
 // Rotas do projeto
 export const routes = [
     { exact: true, path: '/', component: Dashboard },
-    { exact: true, path: '/users', component: User },
-    { exact: true, path: '/profiles', component: Profile }
 ]
 
 // Menu do projeto
 export const menu = {
     '/': { title: 'Dashboard', icon: 'dashboard' },
-    '/users': { title: 'Usuários', icon: 'user' },
-    'profiles': {
-        title: 'Perfis', icon: 'users',
+    // 'profiles': {
+    //     title: 'Perfis', icon: 'users',
         //Exemplo de menu cascateado
         // children: {
         //     '/permissions': { title: 'Permissões', icon: 'user' },
         // }
-    }
+    // }
 }
