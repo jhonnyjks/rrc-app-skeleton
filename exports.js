3e81e3e465ae4dfaa7913602722e555424a9113a
Jhonny Mesquita <jhonnyjks@gmail.com>
2020-04-23 23:23:19 -0300

    Limpando estrutura

--- exports.js
+++ exports.js
@@ -1,39 +1,17 @@
 import Dashboard from './dashboard/dashboard'
-import Menu from './menus/index'
-import { menuReducer } from './menus/duck'
-import { menuPostCategoryReducer } from './menus/menuPostCategory.duck'
-import PostCategory from './post_categories/index'
-import { postCategoryReducer } from './post_categories/duck'
-import Banner from './banners/index'
-import { bannerReducer } from './banners/duck'
-import Post from './posts/index'
-import { postReducer } from './posts/duck'
 
 // Reducers do projeto
 export const reducers = {
-    menu: menuReducer,
-    postCategory: postCategoryReducer,
-    menuPostCategory: menuPostCategoryReducer,
-    banner: bannerReducer,
-    post: postReducer
 }
 
 // Rotas do projeto
 export const routes = [
     { exact: true, path: '/', component: Dashboard },
-    { exact: true, path: '/menus', component: Menu },
-    { exact: true, path: '/post_categories', component: PostCategory },
-    { exact: true, path: '/banners', component: Banner },
-    { exact: true, path: '/posts', component: Post },
 ]
 
 // Menu do projeto
 export const menu = {
     '/': { title: 'Dashboard', icon: 'dashboard' },
-    '/menus': { title: 'Menu', icon: 'book' },
-    '/post_categories': { title: 'Categorias de posts', icon: 'book' },
-    '/banners': { title: 'Banners', icon: 'book' },
-    '/posts': { title: 'Posts', icon: 'book' },
     // 'profiles': {
     //     title: 'Perfis', icon: 'users',
         //Exemplo de menu cascateado
